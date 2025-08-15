5150c9fd87904c1c6822861d099d0eefb2d1a71e
Harshil <harshilgupta.2005@gmail.com>
2025-08-15 18:36:33 +0530

    added google provider

--- src/auth.ts
+++ src/auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import client from "@/lib/db";
 import Resend from "next-auth/providers/resend";
+import Google from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
 	adapter: MongoDBAdapter(client),
@@ -9,6 +10,17 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 		Resend({
 			apiKey: process.env.RESEND_API_KEY,
 			from: process.env.EMAIL_FROM || "",
+		}),
+		Google({
+			clientId: process.env.AUTH_GOOGLE_ID || "",
+			clientSecret: process.env.AUTH_GOOGLE_SECRET || "",
+			authorization: {
+				params: {
+					prompt: "consent",
+					access_type: "offline",
+					response_type: "code",
+				},
+			},
 		}),
 	],
 });
